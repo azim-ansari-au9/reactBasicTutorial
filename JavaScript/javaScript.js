9990417d86196cfdd56d4766e5f4aa5aa51edeb8
Azim Ansari <60268651+azim-ansari-au9@users.noreply.github.com>
2021-01-12 17:33:20 +0530

    JS Tutorial Updated

---
+++ JavaScript/javaScript.js
@@ -0,0 +1,54 @@
+// postFix operator
+
+// var num = 20;
+// var newNum = num++;
+// console.log(num);
+// console.log(newNum);
+////output will  21 and 20.
+
+//prfix operator
+
+// var num = 20;
+// var newNum = ++num;
+// console.log(num);
+// console.log(newNum);
+// //output will be 21 and 21.
+
+//comprision operator
+// var a= 10;
+// var b = 20;
+// console.log(a == b);
+////output wil be false.
+
+// Question
+// Swap  two number a=10 and b=5
+// var a = 10;
+// var b = 5;
+// var c = b;
+// b= a;
+// a= c;
+// console.log(a,b);
+
+// // write a programme for finding the leap year
+// var year = 2024
+// if (year%4 === 0){
+//     if(year%100 === 0){
+//         console.log('leap year'+year)}
+//         else{
+//         console.log('not a leap year'+ year)}
+//     console.log('leap year'+year)}
+//     else{
+//         console.log('not a leap year'+ year)
+//     }
+
+
+// Falsy statement 
+// 0,"",undefined,null,NaN these five are falsy statement
+// if(1){
+//     console.log('hey,you are losser')
+// }else{
+//     console.log('omg, you are the winner')
+// }
+
+////Ternary operator
+// It is javaScript operator which deals with three operands
