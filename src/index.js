ace7e9d8b78cc522b71960723d7de7fa1ad0a1bc
Azim Ansari <60268651+azim-ansari-au9@users.noreply.github.com>
2021-01-27 20:17:46 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import Comp from './Comp';
-
-ReactDOM.render(<Comp />, document.getElementById('root'));
