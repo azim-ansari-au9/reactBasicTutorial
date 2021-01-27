68700c69e5630b458a443ed1fa2286c31aba7517
Azim Ansari <60268651+azim-ansari-au9@users.noreply.github.com>
2021-01-27 20:19:35 +0530

    Add files via upload

---
+++ pokemonReact/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import Comp from './Comp';
+
+ReactDOM.render(<Comp />, document.getElementById('root'));
