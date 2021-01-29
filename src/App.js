a9ebb0eab1c7fcdc11c034ebbf7419d811fe6b4e
Azim Ansari <azimpanjwar@gmail.com>
2021-01-29 20:20:41 +0530

    hook in class component

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  state={
+    count: 0
+  }
+  incNum=()=>{
+    this.setState({
+      count: this.state.count+1
+    });
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+      <h1>Counter App</h1>
+      <button className='btn btn-dark' onClick={this.incNum}>click here {this.state.count}</button>
+      </div>
+    )
+  }
+}
+
+export default App
