1017a134aba82a4d6b686e0e3ee4a5bd6387130d
Azim Ansari <60268651+azim-ansari-au9@users.noreply.github.com>
2021-02-06 18:00:23 +0530

    How to toggle data in hook

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 
 
 const  App = () => {
-  // console.log(useState('azim asnari'))
-  // var val = 'Azim Ansari hooks';
 
-  const [val, setVal] = useState('Azim Ansari hooks')
-  const onChange = () => {
-    setVal(' welcome To Hooks')
-}
+  const [val, setVal] = useState('Azim Ansari hooks');
+  
+  const onChange = (e) => {
+    e.preventDefault();
+    setVal('welcome To Hooks');
+    let name = val;
+    (name === 'Azim Ansari hooks')?
+    setVal('welcome To Hooks'):setVal('Azim Ansari hooks')
+    // clearState()
+  }
 
-// console.log(val)
+console.log(val)
 return (
     <div className="App">
       <h1>{val}</h1>
